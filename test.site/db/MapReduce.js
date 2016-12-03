04818bd1fe2bf834c6808b86de0783bd07e9e224
Sean Sy <seannevintsy@gmail.com>
2016-12-03 18:02:55 +0800

    Add code to MapReduce that checks how data points deviate from the average.

--- test.site/db/MapReduce.js
+++ test.site/db/MapReduce.js
@@ -13,13 +13,25 @@ var finalize = function(key, value){
 	var total = value.adj_close.reduce(function(x,y){return x + y;}, 0);
 	var average = (total / value.adj_close.length);
 	var standard_deviation = 0;
- 	for (var i = 0; i < value.adj_close.length; i++){
- 		standard_deviation += Math.pow((value.adj_close[i] - average), 2); 
-  	}
+	for (var i = 0; i < value.adj_close.length; i++){
+		standard_deviation += Math.pow((value.adj_close[i] - average), 2); 
+	}
 	standard_deviation = Math.sqrt(standard_deviation / value.adj_close.length);
-
-return {total: total, count: value.adj_close.length, average: average, sd: standard_deviation};
+	var within_one_sd = 0;
+	var one_to_two_sd = 0;	
+	var beyond_two_sd = 0;
+	for (var i = 0; i < value.adj_close.length; i++){
+		if(Math.abs(value.adj_close[i] - average) > 2 * standard_deviation){
+			beyond_two_sd++;
+		} else if (Math.abs(value.adj_close[i] - average) <= 2 * standard_deviation && Math.abs(value.adj_close[i] - average) >= standard_deviation) {
+			one_to_two_sd++;
+		} else {
+			within_one_sd++;
+		}
+	}
+	return {adj_close: total, count: value.adj_close.length, average: average, sd: standard_deviation, within_one_sd: within_one_sd, one_to_two_sd: one_to_two_sd, beyond_two_sd: beyond_two_sd};
 }
+
 
 resultsfb = 
 	db.runCommand({
